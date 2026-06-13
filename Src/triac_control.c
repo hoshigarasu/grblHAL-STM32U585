23feb2eba092ac2d979817e4d18f468a830b9700
Koji Tokura <koji@uno-q>
2026-06-13 11:36:42 +0000

    style: add GPLv3 license header to triac_control.c (D7)

--- Src/triac_control.c
+++ Src/triac_control.c
@@ -1,7 +1,26 @@
-/**
- * triac_control.c
- * STM32U585 TRIAC制御実装 — DimmerLink (I2C) + センサー/ファン
- */
+/*
+  triac_control.c - TRIAC power control for STM32U585 (Arduino UNO Q)
+
+  DimmerLink I2C controller + ADC temperature/current sensing + FAN GPIO.
+  Integrated into grblHAL via M810-M816 user M-codes (triac_mcodes.c).
+
+  Part of grblHAL-STM32U585 (https://github.com/hoshigarasu/grblHAL-STM32U585)
+
+  Copyright (c) 2025-2026 Koji Tokura
+
+  grblHAL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grblHAL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "triac_control.h"
 #include "stm32u5xx_hal.h"
